060a5ebd568f7a5ee3bee7ac6a7d57cb94c8c4a0
Jdiazr36 <jdiazr36@gmail.com>
2016-04-08 11:52:32 +0200

    ya entra a la partida

--- app/src/main/java/com/maco/clientejuegos/gui/LoginActivity.java
+++ app/src/main/java/com/maco/clientejuegos/gui/LoginActivity.java
@@ -36,6 +36,8 @@ public class LoginActivity extends AppCompatActivity {
     public void login(View view) {
         if (loginTask!=null)
             return;
+        EditText etIPServer=(EditText) this.findViewById(R.id.etIPServer);
+        Proxy.get().setUrlServer(etIPServer.getText().toString());
         EditText etEmail= (EditText) this.findViewById(R.id.etEmail);
         EditText etPwd= (EditText) this.findViewById(R.id.etPwd);
         this.loginTask=new LoginTask(this, etEmail.getText().toString(), etPwd.getText().toString());
