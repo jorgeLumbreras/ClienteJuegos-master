352c12cd7718474d280b2783d4e5d71fbd5e84a3
Jdiazr36 <jdiazr36@gmail.com>
2016-04-12 17:11:10 +0200

    clase movementMessage

---
+++ app/src/main/java/sudokus/MovementMessage.java
@@ -0,0 +1,44 @@
+package sudokus;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.uclm.esi.common.jsonMessages.JSONMessage;
+import edu.uclm.esi.common.jsonMessages.JSONable;
+
+/**
+ * Created by Jaime on 12/04/2016.
+ */
+public class MovementMessage extends JSONMessage {
+
+    @JSONable
+    private String user;
+    @JSONable
+    private int idMatch;
+    @JSONable
+    private int casilla;
+    @JSONable
+    private String valor;
+
+    public MovementMessage(String user,int idMatch, int casilla, String valor) {
+        super(false);
+        this.user = user;
+        this.idMatch = idMatch;
+        this.casilla = casilla;
+        this.valor = valor;
+    }
+
+    public MovementMessage(JSONObject jso) throws JSONException {
+        this(jso.getString("user"),jso.getInt("idMatch"),jso.getInt("casilla"),jso.getString("valor"));
+    }
+
+    public String getUser() { return this.user; }
+
+    public int getIdMatch() { return this.idMatch; }
+
+    public int getCasilla() { return this.casilla; }
+
+    public String getValor() {
+        return this.valor;
+    }
+}
